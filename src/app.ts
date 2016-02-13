ef1f097af75ddea75b9d724f991469d48169ca3c
Jesse Palmer <jesselpalmer@gmail.com>
2016-02-13 00:45:46 -0500

    various fixes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import MainMenu from './menu/main_menu';
+import * as fs from 'fs';
 
 class App {
   
@@ -6,7 +7,26 @@ class App {
     const mainMenu = new MainMenu();
     mainMenu.printWelcome();
   }
+  
+  openFile(fileName? : string) {
+    if (!process.argv[2]) {
+      fileName = 'game.bas';
+    } else {
+      fileName = process.argv[2];
+    }
+    
+    fs.readFile(fileName, 'utf-8', function(err, data) {
+      if (err) throw err;
+      console.log('OK: ' + fileName);
+      console.log(data)
+    });
+  }
+  
+  startREPL() {
+    console.log('>>>');
+  }
 }
 
 const app = new App();
 app.start();
+app.openFile();
