4e4a5a869de20314e003401ff86e35a19df5537a
Jesse Palmer <jesselpalmer@gmail.com>
2016-03-19 23:30:07 -0400

    fixes code

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,46 @@
-import MainMenu from './menu/main_menu';
+import { MainMenu } from './menu/main_menu';
 import * as fs from 'fs';
 import * as rl from 'readline';
 
 class App {
   
+  constructor() {
+    this.start();
+    this.openFile();
+  }
+  
   start() {
     const mainMenu = new MainMenu();
     mainMenu.printWelcome();
-  }
-  constructor() {
-    
-    this.openFile(fileName? : string) {
-    if (!process.argv[2]) {
-      fileName = 'game.bas';
-    } else {
-      fileName = process.argv[2];
-    }
-    
-    fs.readFile(fileName, 'utf-8', function(err, data) {
-      if (err) throw err;
-      console.log(`OK: ${fileName}`);
-      console.log(`Validating contents of ${fileName}...`);
-      
-      const rd = rl.createInterface({
-        input: fs.createReadStream(fileName),
-        output: process.stdout,
-        terminal: false
-      });
-
-      rd.on('line', function(line) {
-        console.log(`VALID: ${line}`);
-      });
-    });
   }
   
   startREPL() {
     console.log('>>>');
   }
     
+  openFile(fileName? : string) {
+      if (!process.argv[2]) {
+        fileName = 'game.bas';
+      } else {
+        fileName = process.argv[2];
+      }
+      
+      fs.readFile(fileName, 'utf-8', function(err, data) {
+        if (err) throw err;
+        console.log(`OK: ${fileName}`);
+        console.log(`Validating contents of ${fileName}...`);
+        
+        const rd = rl.createInterface({
+          input: fs.createReadStream(fileName),
+          output: process.stdout,
+          terminal: false
+        });
+
+        rd.on('line', function(line) {
+          console.log(`VALID: ${line}`);
+        });
+    });
   }
-  
-  
-  
 }
 
 const app = new App();
-app.start();
-app.openFile();
--- src/menu/main_menu.ts
+++ src/menu/main_menu.ts
@@ -1,4 +1,4 @@
-export default class MainMenu {
+export class MainMenu {
   
   printWelcome() {
     console.log('BASIC 0.0.0 (February, 11, 2016)');
