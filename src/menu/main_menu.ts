7cf3933ca12c833bceebf5ef38cbea4bca6651f6
Jesse Palmer <jesselpalmer@gmail.com>
2016-03-20 16:10:55 -0400

    renamed and updated header

--- src/menu/main_menu.ts
+++
@@ -1,6 +0,0 @@
-export class MainMenu {
-  
-  printWelcome() {
-    console.log('BASIC 0.0.0 (February, 11, 2016)');
-  }
-}
---
+++ src/repl/header.ts
@@ -0,0 +1,8 @@
+export class Header {
+  constructor() {
+  }
+  
+  printWelcome() : void {
+    console.log('BASIC 0.0.0 (March, 20, 2016)');
+  }
+}
