ab863b4a7873d5a92f51e3bd8713d7a261bbfeb2
Jesse Palmer <jesselpalmer@gmail.com>
2020-09-23 18:28:08 -0700

    refactor: code

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,32 @@
-import { REPL } from './repl/repl';
-import { ExecProgram } from './exec_program/exec_program';
+import { REPL } from './repl/repl'
+import { ExecProgram } from './exec_program/exec_program'
 
 class App {
   constructor() {
     this.main()
   }
 
-  private main() {
+  private main(): void {
     const isTooManyArgs = process.argv.length > 3
     const isFilenameInputted = process.argv.length === 3
+    const filenameIndex = 2
 
     if (isTooManyArgs) {
       console.log('Usage: nBASIC [script]')
     } else if (isFilenameInputted) {
-      const filePath : string = process.argv[2]
+      const filePath: string = process.argv[filenameIndex]
       this.runFile(filePath)
     } else {
       this.runPrompt()
     }
   }
 
-  private runFile(filePath : string) : void {
+  private runFile(filePath: string): void {
     const program = new ExecProgram()
     program.start(filePath)
   }
 
-  private runPrompt() : void {
+  private runPrompt(): void {
     const repl = new REPL()
     repl.start()
   }
