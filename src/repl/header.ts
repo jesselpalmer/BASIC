ad21122b0bf7cd2cecf94366b8cbdf9936bc501a
Jesse Palmer <jesselpalmer@gmail.com>
2020-10-17 15:16:01 -0700

    feat: repl works!

--- src/repl/header.ts
+++
@@ -1,5 +0,0 @@
-export default class Header {
-  public printWelcome(): void {
-    console.log(`BASIC 0.0.7 (October 3, 2020)`)
-  }
-}
--- src/repl/repl.ts
+++ src/repl/repl.ts
@@ -1,10 +1,30 @@
-import Header from './header'
+import readline = require('readline')
+import ExecProgram from '../exec_program/exec_program'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 export default class Repl {
   public start(): void {
-    const header = new Header()
-    header.printWelcome()
-        
-    console.log('>>>')
+    const program = new ExecProgram()
+    console.log(`BASIC 0.0.8 (October 17, 2020)`)
+
+    rl.setPrompt(`>>> `)
+    rl.prompt()
+
+    rl.on('line', (line) => {
+      if (line === null || line === '') {
+        console.log(`Have a great day :)`)
+        process.exit(0)
+      }
+      program.readLine(line)
+      rl.prompt()
+    }).on('close', function() {
+      console.log()
+      console.log(`Have a great day :)`)
+      process.exit(0)
+    })
   }
 }
