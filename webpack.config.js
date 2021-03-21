69042489c2b2e5fad0a2dea23cfa844020c54fbd
Jesse Palmer <jesselpalmer@gmail.com>
2021-03-21 06:10:28 +0000

    ci: install webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
