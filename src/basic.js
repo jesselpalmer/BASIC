a938c4f96da58fa54f5fe3ebd0ba62cc825ca7ee
Jesse Palmer <jesselpalmer@gmail.com>
2022-12-19 15:35:16 -0800

    chore: lots of updates

--- src/basic.js
+++ src/basic.js
@@ -1,10 +1,38 @@
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
+
 import constants from './shared/constants';
-import ExecProgram from './exec_program/exec_program';
 import Repl from './repl/repl';
 
 export default class Basic {
   constructor() {
     this.main();
+  }
+
+  run(source) {
+    const tokens = source.split(' ');
+
+    tokens.forEach((token) => {
+      console.log(token);
+    });
+  }
+
+  runPrompt() {
+    const repl = new Repl();
+    repl.start();
+  }
+
+  runFile(filePathStr) {
+    const filePath = path.basename(filePathStr);
+
+    try {
+      const data = fs.readFileSync(filePath, {flag: 'r'});
+      this.run(data.toString(constants.DEFAULT_CHAR_SET));
+    } catch (err) {
+      console.error(err);
+      process.exit(os.constants.errno.ENOENT);
+    }
   }
 
   main() {
@@ -13,23 +41,13 @@ export default class Basic {
     const filenameIndex = 2;
 
     if (isTooManyArgs) {
-      console.log(constants.ErrorMessages.TOO_MANY_ARGS);
-      process.exit(constants.ExitCodes.INVALID_ARGUMENT);
+      console.log(constants.ErrorMessages.E2BIG);
+      process.exit(os.constants.errno.E2BIG);
     } else if (isFilenameInputted) {
       const filePath = process.argv[filenameIndex];
       this.runFile(filePath);
     } else {
       this.runPrompt();
     }
-  }
-
-  runFile(filePath) {
-    const program = new ExecProgram();
-    program.start(filePath);
-  }
-
-  runPrompt() {
-    const repl = new Repl();
-    repl.start();
   }
 }
--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,8 +1,4 @@
 const constants = {
-  ExitCodes: {
-    NORMAL: 0,
-    INVALID_ARGUMENT: 9,
-  },
   Strings: {
     Repl: {
       WELCOME_MESSAGE: 'BASIC 0.0.10 (December 6, 2022)',
@@ -11,8 +7,9 @@ const constants = {
     },
   },
   ErrorMessages: {
-    TOO_MANY_ARGS: 'Usage: nBASIC [script]',
+    E2BIG: 'Usage: nBASIC [script]',
   },
+  DEFAULT_CHAR_SET: 'utf-8',
 };
 
 export default constants;
